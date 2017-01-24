34029bf63bcaac4099f18557ab8706ad7a5b6ae4
William <wravel@Goa13.local>
2017-01-24 13:33:04 +0100

    Update init.el

--- init.el
+++ init.el
@@ -104,6 +104,21 @@
 ;;
 (load-theme 'monokai t)
 
+
+
+
+
+;;
+;;
+;;
+;;
+;;
+;;
+(load-theme 'monokai t)
+
+;; Yascroll
+;; (global-yascroll-bar-mode 1)
+
 (require 'powerline)
 ;;(powerline-default-theme)
 (powerline-center-theme)
@@ -121,6 +136,60 @@
 ;; Remember `er/expand-region' is bound to M-2!
 (global-set-key (kbd "M-3") #'mc/mark-next-like-this)
 (global-set-key (kbd "M-4") #'mc/mark-previous-like-this)
+
+
+
+
+
+
+
+
+
+
+
+
+;; 
+;; Half page scrolling script
+;; https://www.emacswiki.org/emacs/HalfScrolling
+;;
+(defun zz-scroll-half-page (direction)
+    "Scrolls half page up if `direction' is non-nil, otherwise will scroll half page down."
+    (let ((opos (cdr (nth 6 (posn-at-point)))))
+      ;; opos = original position line relative to window
+      (move-to-window-line nil)  ;; Move cursor to middle line
+      (if direction
+          (recenter-top-bottom -1)  ;; Current line becomes last
+        (recenter-top-bottom 0))  ;; Current line becomes first
+      (move-to-window-line opos)))  ;; Restore cursor/point position
+  
+  (defun zz-scroll-half-page-down ()
+    "Scrolls exactly half page down keeping cursor/point position."
+    (interactive)
+    (zz-scroll-half-page nil))
+  
+  (defun zz-scroll-half-page-up ()
+    "Scrolls exactly half page up keeping cursor/point position."
+    (interactive)
+    (zz-scroll-half-page t))
+
+(global-set-key (kbd "M-p") 'zz-scroll-half-page-up) 
+(global-set-key (kbd "M-n") 'zz-scroll-half-page-down) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -312,7 +381,7 @@
  '(git-gutter:handled-backends (quote (git hg bzr svn)))
  '(package-selected-packages
    (quote
-    (workgroups2 multiple-cursors powerline smex magit-svn git-gutter other-frame-window desktop+ bookmark+ smart-mode-line undo-tree expand-region avy-menu ace-jump-mode auto-complete helm-anything ace-window git-gutter+ php-mode php+-mode web-mode magit neotree monokai-theme helm-projectile helm))))
+    (yascroll workgroups2 multiple-cursors powerline smex magit-svn git-gutter other-frame-window desktop+ bookmark+ smart-mode-line undo-tree expand-region avy-menu ace-jump-mode auto-complete helm-anything ace-window git-gutter+ php-mode php+-mode web-mode magit neotree monokai-theme helm-projectile helm))))
 
 
 
