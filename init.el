9c4b0de0cbffcc56ae1600f694c02ae514b41b26
wravel <wravel@iMac-de-William.local>
2017-02-21 17:25:18 +0100

    Update init.el

--- init.el
+++ init.el
@@ -228,6 +228,8 @@
 ;; Liste des packages a installer si repertoire non disponible
 (setq package-list '(
   ;;archives
+  neotree
+  edit-server
   ;;dumb-jump
   ag
 ;;  logview
@@ -852,8 +854,8 @@
  ;; If there is more than one, they won't work right.
  '(bmkp-last-as-first-bookmark-file "~/.emacs.d/bookmarks")
  '(custom-safe-themes
-   (quote
-    ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "98cc377af705c0f2133bb6d340bf0becd08944a588804ee655809da5d8140de6" "4980e5ddaae985e4bae004280bd343721271ebb28f22b3e3b2427443e748cd3f" "9f3181dc1fabe5d58bbbda8c48ef7ece59b01bed606cfb868dd147e8b36af97c" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "9d91458c4ad7c74cf946bd97ad085c0f6a40c370ac0a1cbeb2e3879f15b40553" "dd6e52a5b1180f5c8bf408764a32867e2fa86594ded78a29040cafce6a4ea808" "945fe66fbc30a7cbe0ed3e970195a7ee79ee34f49a86bc96d02662ab449b8134" "ad1c2abad40e11d22156fe3987fd9b74b9e1c822264a07dacb24e0b3133aaed1" "eb0a314ac9f75a2bf6ed53563b5d28b563eeba938f8433f6d1db781a47da1366" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "f5ad3af69f2b6b7c547208b8708d4fa7928b5697ca0845633d1d67c2d145952a" "c7a9a68bd07e38620a5508fef62ec079d274475c8f92d75ed0c33c45fbe306bc" default)))
+        (quote
+         ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "98cc377af705c0f2133bb6d340bf0becd08944a588804ee655809da5d8140de6" "4980e5ddaae985e4bae004280bd343721271ebb28f22b3e3b2427443e748cd3f" "9f3181dc1fabe5d58bbbda8c48ef7ece59b01bed606cfb868dd147e8b36af97c" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "9d91458c4ad7c74cf946bd97ad085c0f6a40c370ac0a1cbeb2e3879f15b40553" "dd6e52a5b1180f5c8bf408764a32867e2fa86594ded78a29040cafce6a4ea808" "945fe66fbc30a7cbe0ed3e970195a7ee79ee34f49a86bc96d02662ab449b8134" "ad1c2abad40e11d22156fe3987fd9b74b9e1c822264a07dacb24e0b3133aaed1" "eb0a314ac9f75a2bf6ed53563b5d28b563eeba938f8433f6d1db781a47da1366" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "f5ad3af69f2b6b7c547208b8708d4fa7928b5697ca0845633d1d67c2d145952a" "c7a9a68bd07e38620a5508fef62ec079d274475c8f92d75ed0c33c45fbe306bc" default)))
  '(git-gutter:added-sign "|")
  '(git-gutter:deleted-sign "|")
  '(git-gutter:modified-sign "|")
@@ -861,14 +863,20 @@
  '(helm-follow-mode-persistent t)
  '(magit-dispatch-arguments nil)
  '(package-selected-packages
-   (quote
-    (monokai-theme color-theme-sanityinc-tomorrow moe-theme material-theme noctilux-theme nlinum crosshairs dumb-mode ac-php theme-doom-molokai doom-molokai zenburn-theme js2-mode tern-auto-complete psvn key-chord php-mode flymake-mode ggtags less-css-mode helm-ag ag dired+ tern diff-hl dired-narrow dired-filter dired-hacks-utils exec-path-from-shell dsvn helm-swoop highlight-symbol zerodark-theme markdown-mode+ smart-tab emmet-mode autopair company web-beautify multiple-cursors powerline other-frame-window desktop+ smart-mode-line undo-tree expand-region avy-menu ace-jump-mode auto-complete helm-anything ace-window web-mode magit helm-projectile helm)))
+        (quote
+         (edit-server neotree elfeed logview monokai-theme color-theme-sanityinc-tomorrow moe-theme material-theme noctilux-theme nlinum crosshairs dumb-mode ac-php theme-doom-molokai doom-molokai zenburn-theme js2-mode tern-auto-complete psvn key-chord php-mode flymake-mode ggtags less-css-mode helm-ag ag dired+ tern diff-hl dired-narrow dired-filter dired-hacks-utils exec-path-from-shell dsvn helm-swoop highlight-symbol zerodark-theme markdown-mode+ smart-tab emmet-mode autopair company web-beautify multiple-cursors powerline other-frame-window desktop+ smart-mode-line undo-tree expand-region avy-menu ace-jump-mode auto-complete helm-anything ace-window web-mode magit helm-projectile helm)))
  '(yas-global-mode t t))
 
 
 
 
 
+;; Elfeed
+(global-set-key (kbd "C-x w") 'elfeed)
+(setq elfeed-feeds
+      '(("http://nullprogram.com/feed/" blog emacs)
+        "http://www.50ply.com/atom.xml"  ; no autotagging
+        ("http://nedroid.com/feed/" webcomic)))    
 
 
 
@@ -1143,7 +1151,7 @@
 
 ;; Desktop+ Configuration
 ;; Ajout de save mode auto
-(desktop-save-mode 1)
+;; (desktop-save-mode 1)
 
 
 
@@ -1159,11 +1167,19 @@
 
 
 
-;; (require 'neotree)
-;; (global-set-key [f8] 'neotree-toggle)
-;;(setq neo-smart-open t)
+(require 'neotree)
+(global-set-key [f8] 'neotree-toggle)
+(setq neo-smart-open t)
 
 
+;; Edit server
+;;     
+;;(require 'edit-server')
+;;(edit-server-start)        
+
+
+
+    
 ;; Web mode
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.html\\'" . nxml-mode))
@@ -1193,6 +1209,8 @@
 )
 (add-hook 'web-mode-hook  'my-web-mode-hook)
 
+(setq web-mode-enable-current-element-highlight t)
+(setq web-mode-enable-current-column-highlight t)    
 
 
 
@@ -1245,30 +1263,6 @@
 ;; undo-tree
 (require 'undo-tree)
 (global-undo-tree-mode)
-
-
-;; CUA MODE (integre par default) permet de faire du ctrl c/v/x a la place des yank bidul pouet
-;; Update : C-v entre en conflit avec la fonction de base de scroll
-;; (cua-mode t)
-;;    (setq cua-auto-tabify-rectangles nil) ;; Don't tabify after rectangle commands
-;;    (transient-mark-mode 1) ;; No region when it is not highlighted
-;;    (setq cua-keep-region-after-copy t) ;; Standard Windows behaviour
-
-
-
-;; Mac key bindings
-;;(setq mac-command-modifier 'meta) ; make cmd key do Meta
-;;(setq mac-option-modifier 'super) ; make opt key do Super
-;;(setq mac-control-modifier 'control) ; make Control key do Control
-;;(setq ns-function-modifier 'hyper)  ; make Fn key do Hyper
-
- ;; changing the command to control
-;;    (setq mac-command-modifier 'control)
-
-
-;; TODO REMETTRE LE MOVE DOWN (avant meta+p) --> le mettre sur CMD+
-;;    --> semble dejà bon ?
-;; TODO : Desactiver CMD+SPACE DANS EMACS ()
 
 
 
