093f3c4a3c3b29198f413556f61c3bf288ed4ed2
William Ravel <wravel@gmail.com>
2017-05-25 01:09:11 +0200

    Move from elpa to vendor

---
+++ vendor/avy-menu/avy-menu-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "avy-menu" "0.1.1" "Library providing avy-powered popup menu" '((emacs "24.3") (avy "0.3.0")) :commit "0560e57d7f07b655aa73946f5a2066a42c3af0b7" :url "https://github.com/mrkkrp/avy-menu" :keywords '("popup" "menu"))
