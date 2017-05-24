08033951315faeebb4bbf02fb6c7dbaebc4bb891
William Ravel <wravel@gmail.com>
2017-05-24 19:53:29 +0200

    Update conf

--- include/package_list.el
+++ include/package_list.el
@@ -205,3 +205,4 @@
 (add-to-list 'load-path "~/.emacs.d/plugins/restclient")
 (add-to-list 'load-path "~/.emacs.d/plugins/diff-hl")
 (add-to-list 'load-path "~/.emacs.d/plugins/org-mouse")
+(add-to-list 'load-path "~/.emacs.d/plugins/workgroups2")
