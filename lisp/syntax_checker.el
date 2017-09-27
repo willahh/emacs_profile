bfb9b2d91ff750e438c96ef81940edabf494112f
William Ravel <wravel@gmail.com>
2017-09-28 00:49:31 +0200

    Update flycheck config

--- lisp/syntax_checker.el
+++ lisp/syntax_checker.el
@@ -4,11 +4,6 @@
 (require 'flycheck)
 (require 'flycheck-pos-tip)
 
-;; Note : doit etre active de maniere globale, je n arrive pas
-;; a l activer avec un hook sur js2mode (pour le moment)
-;; (global-flycheck-mode)
-;; Update : fonctionel avec flycheck-mode
-
 (add-hook 'php-mode-hook 'flycheck-mode)
 (add-hook 'php-mode-hook 'flycheck-pos-tip-mode)
 
@@ -41,6 +36,28 @@
       (let ((web-mode-enable-part-face nil))
         ad-do-it)
     ad-do-it))
+
+;; From magnars
+(defun magnars/adjust-flycheck-automatic-syntax-eagerness ()
+  "Adjust how often we check for errors based on if there are any.
+
+This lets us fix any errors as quickly as possible, but in a
+clean buffer we're an order of magnitude laxer about checking."
+  (setq flycheck-idle-change-delay
+        (if flycheck-current-errors 0.5 30.0)))
+
+;; Each buffer gets its own idle-change-delay because of the
+;; buffer-sensitive adjustment above.
+(make-variable-buffer-local 'flycheck-idle-change-delay)
+
+(add-hook 'flycheck-after-syntax-check-hook
+          'magnars/adjust-flycheck-automatic-syntax-eagerness)
+
+;; Remove newline checks, since they would trigger an immediate check
+;; when we want the idle-change-delay to be in effect while editing.
+(setq flycheck-check-syntax-automatically '(save
+                                            idle-change
+                                            mode-enabled))
 
 ;; editorconfig
 (require 'editorconfig)
