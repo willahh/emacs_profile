dda3eca12599664300f425f311aa09215e2bfffb
William Ravel <wravel@gmail.com>
2017-09-28 00:45:13 +0200

    Add some clojure setup

--- lisp/language/clojure.el
+++ lisp/language/clojure.el
@@ -9,6 +9,17 @@
 (require 'helm-cider)
 (require 'clojure-snippets)
 
+;; From Magnar
+(defun clj-duplicate-top-level-form ()
+  (interactive)
+  (save-excursion
+    (cljr--goto-toplevel)
+    (insert (cljr--extract-sexp) "\n")
+    (cljr--just-one-blank-line)))
+
+(define-key clojure-mode-map (kbd "M-s-d") 'clj-duplicate-top-level-form)
+
+;; 
 (setq cider-prompt-for-symbol nil)
 
 ;; https://markhudnall.com/2016/04/25/starting-figwheel-in-emacs/
@@ -41,4 +52,77 @@
 
 
 
+;; From Magnar
+;; Use figwheel for cljs repl
+(setq cider-cljs-lein-repl "(do (use 'figwheel-sidecar.repl-api) (start-figwheel!) (cljs-repl))")
 
+;; Indent and highlight more commands
+(put-clojure-indent 'match 'defun)
+
+;; Enable error buffer popping also in the REPL:
+(setq cider-repl-popup-stacktraces t)
+
+;; Specify history file
+(setq cider-history-file "~/.emacs.d/nrepl-history")
+
+;; auto-select the error buffer when it's displayed
+(setq cider-auto-select-error-buffer t)
+
+;; Prevent the auto-display of the REPL buffer in a separate window after connection is established
+(setq cider-repl-pop-to-buffer-on-connect nil)
+
+;; Pretty print results in repl
+(setq cider-repl-use-pretty-printing t)
+
+;; Don't prompt for symbols
+(setq cider-prompt-for-symbol nil)
+
+;; Cycle between () {} []
+
+(defun live-delete-and-extract-sexp ()
+  "Delete the sexp and return it."
+  (interactive)
+  (let* ((begin (point)))
+    (forward-sexp)
+    (let* ((result (buffer-substring-no-properties begin (point))))
+      (delete-region begin (point))
+      result)))
+
+(defun live-cycle-clj-coll ()
+  "convert the coll at (point) from (x) -> {x} -> [x] -> (x) recur"
+  (interactive)
+  (let* ((original-point (point)))
+    (while (and (> (point) 1)
+                (not (equal "(" (buffer-substring-no-properties (point) (+ 1 (point)))))
+                (not (equal "{" (buffer-substring-no-properties (point) (+ 1 (point)))))
+                (not (equal "[" (buffer-substring-no-properties (point) (+ 1 (point))))))
+      (backward-char))
+    (cond
+     ((equal "(" (buffer-substring-no-properties (point) (+ 1 (point))))
+      (insert "{" (substring (live-delete-and-extract-sexp) 1 -1) "}"))
+     ((equal "{" (buffer-substring-no-properties (point) (+ 1 (point))))
+      (insert "[" (substring (live-delete-and-extract-sexp) 1 -1) "]"))
+     ((equal "[" (buffer-substring-no-properties (point) (+ 1 (point))))
+      (insert "(" (substring (live-delete-and-extract-sexp) 1 -1) ")"))
+     ((equal 1 (point))
+      (message "beginning of file reached, this was probably a mistake.")))
+    (goto-char original-point)))
+
+(define-key clojure-mode-map (kbd "C-^") 'live-cycle-clj-coll)
+
+;; Warn about missing nREPL instead of doing stupid things
+
+(defun nrepl-warn-when-not-connected ()
+  (interactive)
+  (message "Oops! You're not connected to an nREPL server. Please run M-x cider or M-x cider-jack-in to connect."))
+
+
+(define-key clojure-mode-map (kbd "C-M-x")   'nrepl-warn-when-not-connected)
+(define-key clojure-mode-map (kbd "C-x C-e") 'nrepl-warn-when-not-connected)
+(define-key clojure-mode-map (kbd "C-c C-e") 'nrepl-warn-when-not-connected)
+(define-key clojure-mode-map (kbd "C-c C-l") 'nrepl-warn-when-not-connected)
+(define-key clojure-mode-map (kbd "C-c C-r") 'nrepl-warn-when-not-connected)
+(define-key clojure-mode-map (kbd "C-c C-z") 'nrepl-warn-when-not-connected)
+(define-key clojure-mode-map (kbd "C-c C-k") 'nrepl-warn-when-not-connected)
+(define-key clojure-mode-map (kbd "C-c C-n") 'nrepl-warn-when-not-connected)
+(define-key clojure-mode-map (kbd "C-c C-q") 'nrepl-warn-when-not-connected)
