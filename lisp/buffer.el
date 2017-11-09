da83dd62b0495dd9d11cfecc73a7e5b3f9ce5082
William Ravel <wravel@gmail.com>
2017-11-09 01:26:54 +0100

    Add ibuffer-vc

--- lisp/buffer.el
+++ lisp/buffer.el
@@ -8,3 +8,12 @@
         ;; ("*shell*"            . (display-buffer-same-window . nil))
         ("*Google Translate*" . (display-buffer-same-window . nil))
         ))
+
+
+;; ibuffer-vc from purcell conf
+;; https://raw.githubusercontent.com/purcell/ibuffer-vc/master/ibuffer-vc.el
+(add-hook 'ibuffer-hook
+    (lambda ()
+      (ibuffer-vc-set-filter-groups-by-vc-root)
+      (unless (eq ibuffer-sorting-mode 'alphabetic)
+        (ibuffer-do-sort-by-alphabetic))))
