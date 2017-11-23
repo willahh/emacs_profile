4a19461a79f0fd08989d82850bb2749cad6be335
wravel <wravel@wr.local>
2017-11-23 13:46:41 +0100

    Add pdf-tools config

--- lisp/misc.el
+++ lisp/misc.el
@@ -767,3 +767,16 @@ Version 2016-10-24"
 (require 'visual-regexp)
 (global-set-key (kbd "M-%") 'vr/query-replace)
 
+
+;; http://pragmaticemacs.com/
+(use-package pdf-tools
+ :pin manual ;; manually update
+ :config
+ ;; initialise
+ (pdf-tools-install)
+ ;; open pdfs scaled to fit page
+ (setq-default pdf-view-display-size 'fit-page)
+ ;; automatically annotate highlights
+ (setq pdf-annot-activate-created-annotations t)
+ ;; use normal isearch
+ (define-key pdf-view-mode-map (kbd "C-s") 'isearch-forward))
