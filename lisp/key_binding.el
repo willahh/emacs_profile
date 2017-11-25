14c5fb1b7e73b0ca54df215c693e43c6aa9c28d3
William Ravel <wravel@gmail.com>
2017-11-25 19:43:59 +0100

    Clean

--- lisp/key_binding.el
+++ lisp/key_binding.el
@@ -813,7 +813,8 @@
 
 
 ;; http://oremacs.com/swiper/
-(global-set-key (kbd "C-s") 'swiper)
+;; (global-set-key (kbd "C-s") 'swiper)
+
 (global-set-key (kbd "M-x") 'counsel-M-x)
 (global-set-key (kbd "C-x C-f") 'counsel-find-file)
 (global-set-key (kbd "<f1> f") 'counsel-describe-function)
