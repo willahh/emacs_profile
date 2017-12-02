0914d25ce51e8f0f2d41127d3034c6987f47b806
William Ravel <wravel@gmail.com>
2017-12-03 00:20:12 +0100

    web-mode-map C-c C-s swiper

--- lisp/key_binding.el
+++ lisp/key_binding.el
@@ -684,6 +684,7 @@
 ;; (global-set-key (kbd "s-s") 'helm-swoop)
 ;; (global-set-key (kbd "s-s") 'swiper)
 (global-set-key (kbd "C-c C-s") 'swiper)
+(define-key web-mode-map (kbd "C-c C-s") 'swiper)
 
 ;; (global-set-key (kbd "s-s") 'helm-swoop)
 ;; (global-set-key (kbd "C-c C-s") 'helm-swoop)
