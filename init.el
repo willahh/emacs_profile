3748dc9c64d4b9f98ad41e02510967ad23b6e0a0
William Ravel <wravel@gmail.com>
2017-12-20 00:35:41 +0100

    Update theme

--- init.el
+++ init.el
@@ -37,35 +37,41 @@
 (load-file "~/.emacs.d/lisp/language/react.el")
 (load-file "~/.emacs.d/lisp/browser.el")
 (load-file "~/.emacs.d/lisp/key_binding.el")
-(load-file "~/.emacs.d/lisp/theme-dark2.el")
+;; (load-file "~/.emacs.d/lisp/theme-dark2.el")
+;; (load-file "~/.emacs.d/lisp/theme-light2.el")
+(load-file "~/.emacs.d/lisp/theme-light3.el")
 
 
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(helm-source-names-using-follow
-   (quote
-    ("Search at ~/www/project/easy3p/easy3p_3.2_ao/dev/module/dashboard/" "Search at ~/.emacs.d/lisp/")))
- '(package-selected-packages
-   (quote
-    (ido-occur ivy-hydra zop-to-char yaml-mode whole-line-or-region which-key wgrep websocket web-mode web-beautify visual-regexp validate use-package toc-org tide syslog-mode sx sourcemap smooth-scrolling sml-mode smex skewer-mode resize-window react-snippets rainbow-mode rainbow-delimiters psysh projectile-ripgrep powerline popwin php-auto-yasnippets perspective pdf-tools paredit-everywhere paradox pandoc-mode ov org-bullets nlinum neotree move-text monokai-theme magit key-chord json-mode js2-refactor js-comint jabber iy-go-to-char intellij-theme inf-clojure indent-guide impatient-mode imenu-list image-dired+ iflipb iedit ido-vertical-mode ido-ubiquitous ido-at-point ibuffer-vc highlight-symbol helm-swoop helm-cider helm-ag gradle-mode google-translate git-timemachine git-gutter ggtags fullframe flycheck-pos-tip flx-ido find-file-in-project expand-region exec-path-from-shell evil-visualstar evil-surround evil-snipe evil-org evil-nerd-commenter evil-matchit evil-leader emmet-mode editorconfig edit-server eclim dtrt-indent dsvn drag-stuff dracula-theme diff-hl darkroom crux counsel company-web company-tern company-quickhelp color-identifiers-mode coffee-mode clojure-snippets clojure-cheatsheet clj-refactor browse-kill-ring avy-menu autopair auto-yasnippet auto-complete anzu ag actionscript-mode ace-window ace-link ace-jump-mode))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((((class color) (min-colors 89)) (:background "#282a36" :foreground "#f8f8f2"))))
- '(rainbow-delimiters-depth-1-face ((t (:foreground "#5abeee"))))
- '(rainbow-delimiters-depth-2-face ((t (:foreground "#ff00a0"))))
- '(rainbow-delimiters-depth-3-face ((t (:foreground "#d2ff00"))))
- '(rainbow-delimiters-depth-4-face ((t (:foreground "#66ff66"))))
- '(rainbow-delimiters-depth-5-face ((t (:foreground "#00ffcc"))))
- '(rainbow-delimiters-depth-6-face ((t (:foreground "#cc99ff"))))
- '(rainbow-delimiters-depth-7-face ((t (:foreground "#fa75e2"))))
- '(rainbow-delimiters-depth-8-face ((t (:foreground "#55bdf0")))))
+
+;; (custom-set-variables
+;;  ;; custom-set-variables was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  '(custom-safe-themes
+;;    (quote
+;;     ("b2c8ea4a3049e4399a4015f8eddb74fcd0473e9aa0c8818a04979230b22d0e75" default)))
+;;  '(helm-source-names-using-follow
+;;    (quote
+;;     ("Search at ~/www/project/easy3p/easy3p_3.2_ao/dev/module/dashboard/" "Search at ~/.emacs.d/lisp/")))
+;;  '(package-selected-packages
+;;    (quote
+;;     (ido-occur ivy-hydra zop-to-char yaml-mode whole-line-or-region which-key wgrep websocket web-mode web-beautify visual-regexp validate use-package toc-org tide syslog-mode sx sourcemap smooth-scrolling sml-mode smex skewer-mode resize-window react-snippets rainbow-mode rainbow-delimiters psysh projectile-ripgrep powerline popwin php-auto-yasnippets perspective pdf-tools paredit-everywhere paradox pandoc-mode ov org-bullets nlinum neotree move-text monokai-theme magit key-chord json-mode js2-refactor js-comint jabber iy-go-to-char intellij-theme inf-clojure indent-guide impatient-mode imenu-list image-dired+ iflipb iedit ido-vertical-mode ido-ubiquitous ido-at-point ibuffer-vc highlight-symbol helm-swoop helm-cider helm-ag gradle-mode google-translate git-timemachine git-gutter ggtags fullframe flycheck-pos-tip flx-ido find-file-in-project expand-region exec-path-from-shell evil-visualstar evil-surround evil-snipe evil-org evil-nerd-commenter evil-matchit evil-leader emmet-mode editorconfig edit-server eclim dtrt-indent dsvn drag-stuff dracula-theme diff-hl darkroom crux counsel company-web company-tern company-quickhelp color-identifiers-mode coffee-mode clojure-snippets clojure-cheatsheet clj-refactor browse-kill-ring avy-menu autopair auto-yasnippet auto-complete anzu ag actionscript-mode ace-window ace-link ace-jump-mode))))
+;; (custom-set-faces
+;;  ;; custom-set-faces was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  '(default ((((class color) (min-colors 89)) (:background "#282a36" :foreground "#f8f8f2"))))
+;;  '(rainbow-delimiters-depth-1-face ((t (:foreground "#5abeee"))))
+;;  '(rainbow-delimiters-depth-2-face ((t (:foreground "#ff00a0"))))
+;;  '(rainbow-delimiters-depth-3-face ((t (:foreground "#d2ff00"))))
+;;  '(rainbow-delimiters-depth-4-face ((t (:foreground "#66ff66"))))
+;;  '(rainbow-delimiters-depth-5-face ((t (:foreground "#00ffcc"))))
+;;  '(rainbow-delimiters-depth-6-face ((t (:foreground "#cc99ff"))))
+;;  '(rainbow-delimiters-depth-7-face ((t (:foreground "#fa75e2"))))
+;;  '(rainbow-delimiters-depth-8-face ((t (:foreground "#55bdf0")))))
 
 ;; Enable window-divier
 (window-divider-mode-apply t)
---
+++ lisp/theme-light3.el
@@ -0,0 +1,1 @@
+(load-file "~/.emacs.d/lisp/theme/tango-plus/tangotango-plus-theme.el")
