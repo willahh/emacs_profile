d00e8f1e7632b7a66ce0c13192e15e5cc063570e
William Ravel <wravel@gmail.com>
2017-12-29 00:35:49 +0100

    Update conf

--- init.el
+++ init.el
@@ -55,20 +55,24 @@
     ("Search at ~/.emacs.d/lisp/" "Search at ~/.emacs.d/lisp/language/")))
  '(package-selected-packages
    (quote
-    (shell-pop visual-regexp-steroids helm-projectile all-the-icons zop-to-char zenburn-theme yaml-mode xcscope whole-line-or-region whitespace-cleanup-mode which-key wgrep-ag websocket web-mode web-beautify visual-regexp validate use-package toc-org tide syslog-mode sx sr-speedbar spaceline sourcemap smooth-scrolling sml-mode smex skewer-mode resize-window react-snippets rainbow-mode rainbow-delimiters psysh projectile-ripgrep popwin php-eldoc php-auto-yasnippets perspective pdf-tools paredit-everywhere paradox pandoc-mode pandoc ov outline-toc org-bullets nodejs-repl nlinum neotree move-text monokai-theme memoize lsp-mode key-chord json-mode js2-refactor js-comint jade-mode jabber iy-go-to-char ivy-hydra intellij-theme inf-clojure indent-guide impatient-mode imenu-list image-dired+ iflipb iedit ido-vertical-mode ido-ubiquitous ido-occur ido-gnus ido-at-point ibuffer-vc highlight-symbol helm-swoop helm-cider helm-ag gradle-mode google-translate google-maps git-timemachine git-gutter ggtags fullframe flycheck-pos-tip flx-ido find-file-in-project fill-column-indicator eyebrowse expand-region exec-path-from-shell evil-visualstar evil-surround evil-snipe evil-org evil-nerd-commenter evil-matchit evil-magit evil-leader emmet-mode editorconfig edit-server eclim ecb dtrt-indent dsvn drag-stuff dracula-theme dired-subtree dired-ranger diff-hl darkroom csv-mode crux counsel company-web company-tern company-quickhelp color-identifiers-mode coffee-mode clojure-snippets clojure-cheatsheet clj-refactor browse-kill-ring autopair auto-yasnippet auto-complete anzu aggressive-indent ag actionscript-mode ace-window ace-popup-menu ace-link ace-jump-mode))))
+    (shell-pop visual-regexp-steroids helm-projectile all-the-icons zop-to-char zenburn-theme yaml-mode xcscope whole-line-or-region whitespace-cleanup-mode which-key wgrep-ag websocket web-mode web-beautify visual-regexp validate use-package toc-org tide syslog-mode sx sr-speedbar spaceline sourcemap smooth-scrolling sml-mode smex skewer-mode resize-window react-snippets rainbow-mode rainbow-delimiters psysh projectile-ripgrep popwin php-eldoc php-auto-yasnippets perspective pdf-tools paredit-everywhere paradox pandoc-mode pandoc ov outline-toc org-bullets nodejs-repl nlinum neotree move-text monokai-theme memoize lsp-mode key-chord json-mode js2-refactor js-comint jade-mode jabber iy-go-to-char ivy-hydra intellij-theme inf-clojure indent-guide impatient-mode imenu-list image-dired+ iflipb iedit ido-vertical-mode ido-ubiquitous ido-occur ido-gnus ido-at-point ibuffer-vc highlight-symbol helm-swoop helm-cider helm-ag gradle-mode google-translate google-maps git-timemachine git-gutter ggtags fullframe flycheck-pos-tip flx-ido find-file-in-project fill-column-indicator eyebrowse expand-region exec-path-from-shell evil-visualstar evil-surround evil-snipe evil-org evil-nerd-commenter evil-matchit evil-magit evil-leader emmet-mode editorconfig edit-server eclim ecb dtrt-indent dsvn drag-stuff dracula-theme dired-subtree dired-ranger diff-hl darkroom csv-mode crux counsel company-web company-tern company-quickhelp color-identifiers-mode coffee-mode clojure-snippets clojure-cheatsheet clj-refactor browse-kill-ring autopair auto-yasnippet auto-complete anzu aggressive-indent ag actionscript-mode ace-window ace-popup-menu ace-link ace-jump-mode)))
+ '(shell-pop-shell-type (quote ("eshell" "*eshell*" (lambda nil (eshell)))))
+ '(shell-pop-term-shell "/bin/bash")
+ '(shell-pop-universal-key "C-t")
+ '(shell-pop-window-position "bottom")
+ '(shell-pop-window-size 30))
 
-;; (custom-set-faces
-;;  ;; custom-set-faces was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  '(default ((((class color) (min-colors 89)) (:foreground "#000000" :background "#ffffff"))))
-;;  '(rainbow-delimiters-depth-1-face ((t (:foreground "#5abeee" :weight bold))))
-;;  '(rainbow-delimiters-depth-2-face ((t (:foreground "#ff00a0" :weight bold))))
-;;  '(rainbow-delimiters-depth-3-face ((t (:foreground "#ff953f" :weight bold))))
-;;  '(rainbow-delimiters-depth-4-face ((t (:foreground "#328d2e" :weight bold))))
-;;  '(rainbow-delimiters-depth-5-face ((t (:foreground "#22988a" :weight bold))))
-;;  '(rainbow-delimiters-depth-6-face ((t (:foreground "#cc99ff" :weight bold))))
-;;  '(rainbow-delimiters-depth-7-face ((t (:foreground "#fa75e2" :weight bold))))
-;;  '(rainbow-delimiters-depth-8-face ((t (:foreground "#55bdf0" :weight bold)))))
-;; (put 'dired-find-alternate-file 'disabled nil)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((((class color) (min-colors 89)) (:foreground "#000000" :background "#ffffff"))))
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "#5abeee" :weight bold))))
+ '(rainbow-delimiters-depth-2-face ((t (:foreground "#ff00a0" :weight bold))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "#ff953f" :weight bold))))
+ '(rainbow-delimiters-depth-4-face ((t (:foreground "#328d2e" :weight bold))))
+ '(rainbow-delimiters-depth-5-face ((t (:foreground "#22988a" :weight bold))))
+ '(rainbow-delimiters-depth-6-face ((t (:foreground "#cc99ff" :weight bold))))
+ '(rainbow-delimiters-depth-7-face ((t (:foreground "#fa75e2" :weight bold))))
+ '(rainbow-delimiters-depth-8-face ((t (:foreground "#55bdf0" :weight bold)))))
