1c6172b33986e7c427a5b062d3be5683ed1ac942
William Ravel <wravel@gmail.com>
2017-12-29 00:59:01 +0100

    Update keybinding

--- lisp/key_binding.el
+++ lisp/key_binding.el
@@ -183,6 +183,7 @@
 (define-key dired-mode-map (kbd "C-c C-p") 'dired-subtree-up)
 (define-key dired-mode-map (kbd "C-c C-n") 'dired-subtree-down)
 (define-key dired-mode-map (kbd "C-x n") 'dired-narrow)
+(define-key dired-mode-map (kbd "b") 'crux-open-with)
 
 ;; Multi cursor stuf
 (global-set-key (kbd "ı") 'mc/mark-next-lines) ; ALT+SHIFT+p
