c2b76b112fd9e240df8e30e880bbe80a32e24131
William Ravel <wravel@gmail.com>
2017-12-29 20:05:10 +0100

    pdf-tools

--- lisp/misc.el
+++ lisp/misc.el
@@ -771,17 +771,17 @@ Version 2016-10-24"
 
 
 ;; http://pragmaticemacs.com/
-;; (use-package pdf-tools
-;;  :pin manual ;; manually update
-;;  :config
-;;  ;; initialise
-;;  (pdf-tools-install)
-;;  ;; open pdfs scaled to fit page
-;;  (setq-default pdf-view-display-size 'fit-page)
-;;  ;; automatically annotate highlights
-;;  (setq pdf-annot-activate-created-annotations t)
-;;  ;; use normal isearch
-;;  (define-key pdf-view-mode-map (kbd "C-s") 'isearch-forward))
+(use-package pdf-tools
+  :pin manual ;; manually update
+  :config
+  ;; initialise
+  (pdf-tools-install)
+  ;; open pdfs scaled to fit page
+  (setq-default pdf-view-display-size 'fit-page)
+  ;; automatically annotate highlights
+  (setq pdf-annot-activate-created-annotations t)
+  ;; use normal isearch
+  (define-key pdf-view-mode-map (kbd "C-s") 'isearch-forward))
 
 
 
