57525a493b592a327ba12df420a275323194914a
William Ravel <wravel@gmail.com>
2017-12-30 23:03:32 +0100

    Update helm conf

--- lisp/navigation.el
+++ lisp/navigation.el
@@ -150,12 +150,21 @@
 (setq ido-everywhere 1)
 ; (setq ido-ubiquitous-mode 1)
 (setq ido-enable-flex-matching 1)
+
+;; Helm conf
 (setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
       helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
       helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
       helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
       helm-ff-file-name-history-use-recentf t
       helm-echo-input-in-header-line t)
+
+(setq helm-M-x-fuzzy-match t
+      helm-recentf-fuzzy-match t
+      helm-buffers-fuzzy-matching t
+      helm-locate-fuzzy-match nil
+      helm-imenu-fuzzy-match t
+      helm-mode-fuzzy-match t)
 
 ;; helm
 (setq helm-autoresize-max-height 500)
