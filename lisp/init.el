9c992dad1171ab47c9d8841cf6655cabdae969a2
wravel <wravel@wr.local>
2018-08-14 14:04:16 +0200

    Update

---
+++ lisp/init.el
@@ -0,0 +1,13 @@
+;; -------- Init
+;; Show matching parenthesis.
+(show-paren-mode)
+
+;; Active winner-mode to allow navigation between different windows.
+(winner-mode t)
+
+;; Disable tabbar mode
+(tabbar-mode 0)
+
+;; Start an eshell process at start-up.
+(eshell)
+
