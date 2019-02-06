6c3f57367e35c74e4320c064f73d40915eb657ed
William Ravel <wravel@gmail.com>
2019-02-06 19:45:59 +0100

    Remove temp elpa directory

--- elpa___/auto-yasnippet-20180503.1908/auto-yasnippet-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "auto-yasnippet" "20180503.1908" "Quickly create disposable yasnippets" '((yasnippet "0.8.0")) :commit "623734aa418b18ff52cb65a0adb9e359aed31615" :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/auto-yasnippet")
--- elpa___/swiper-20180813.1625/swiper-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "swiper" "20180813.1625" "Isearch with an overview. Oh, man!" '((emacs "24.1") (ivy "0.9.0")) :commit "84e7a13f80198a4272c6e0b0b2e3039e710885e7" :keywords '("matching") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
