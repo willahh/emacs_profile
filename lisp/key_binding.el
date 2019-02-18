84d005e6c0913e995cec19f3889260b937bde19e
William Ravel <wravel@gmail.com>
2019-02-18 23:23:16 +0100

    Update

--- lisp/key_binding.el
+++ lisp/key_binding.el
@@ -383,8 +383,8 @@
 
 ;; ---------------- Magit
 (global-set-key (kbd "C-x g") 'magit-status)
-(global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
-(define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
+;; (global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
+;; (define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
 
 ;; Emmet
 (global-set-key [control command shift j] 'emmet-expand-line)
