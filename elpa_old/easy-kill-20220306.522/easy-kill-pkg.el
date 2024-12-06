25985d9307e043164377d8d0117cd8562d944c9a
William Ravel <wravel@gmail.com>
2024-12-06 14:43:03 +0100

    Wip conf with Apple m3

--- elpa_old/easy-kill-20220306.522/easy-kill-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from easy-kill.el  -*- no-byte-compile: t -*-
-(define-package "easy-kill" "20220306.522" "kill & mark things easily" '((emacs "25") (cl-lib "0.5")) :commit "f155d19c528e27f8f6c72f0d75f652edbdcab37f" :authors '(("Leo Liu" . "sdl.web@gmail.com")) :maintainer '("Leo Liu" . "sdl.web@gmail.com") :keywords '("killing" "convenience") :url "https://github.com/leoliu/easy-kill")
--- elpa_old/ido-at-point-20151021.757/ido-at-point-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from ido-at-point.el  -*- no-byte-compile: t -*-
-(define-package "ido-at-point" "20151021.757" "ido-style completion-at-point" '((emacs "24")) :commit "e5907bbe8a3d148d07698b76bd994dc3076e16ee" :authors '(("katspaugh")) :maintainer '("katspaugh") :keywords '("convenience" "abbrev") :url "https://github.com/katspaugh/ido-at-point")
